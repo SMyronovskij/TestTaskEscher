6a4d9588f5e783c39cf8cc23270fe16c9685cab7
Severyn Myronovskij <severyn92@gmail.com>
2022-02-11 16:00:28 +0200

    Fix for test project

--- TestTaskEscher/TestTaskEscher.Tests/Providers/DataProcessingProviderTest.cs
+++ TestTaskEscher/TestTaskEscher.Tests/Providers/DataProcessingProviderTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Moq;
 using TestTaskEscher.DataModel;
 using TestTaskEscher.DataModels.DbModels.PersonModels;
--- TestTaskEscher/TestTaskEscher.Tests/Providers/DataProviders/TextFileDataProviderTest.cs
+++ TestTaskEscher/TestTaskEscher.Tests/Providers/DataProviders/TextFileDataProviderTest.cs
@@ -7,79 +7,80 @@ using TestTaskEscher.DataModel;
 using TestTaskEscher.DataModels.DbModels.PersonModels;
 using TestTaskEscher.Providers.DataProviders;
 
-namespace TestTaskEscher.Tests.Providers.DataProviders;
-
-[ExcludeFromCodeCoverage]
-public class TextFileDataProviderTest
+namespace TestTaskEscher.Tests.Providers.DataProviders
 {
-    private IDataProvider _dataProvider;
-
-    public TextFileDataProviderTest()
+    [ExcludeFromCodeCoverage]
+    public class TextFileDataProviderTest
     {
-        _dataProvider = new TextFileDataProvider();
-    }
+        private IDataProvider _dataProvider;
 
-    [Fact]
-    public async void CreatePersonTest()
-    {
-        var person = new Person
+        public TextFileDataProviderTest()
         {
-            FirstName = "Test1",
-            Surname = "Test2",
-            DateOfBirth = new DateTime(1992, 11, 25),
-            RegistrationAllowed = true
-        };
+            _dataProvider = new TextFileDataProvider();
+        }
 
-        await _dataProvider.CreatePerson(person);
-
-        var personLastLine = File.ReadLines(@"C:\people\mainFile.txt").Last();
-
-        var data = personLastLine.Split("|");
-
-        Assert.False(string.IsNullOrEmpty(data[0]));
-        Assert.Equal(person.FirstName,data[1]);
-        Assert.Equal(person.Surname,data[2]);
-        Assert.Equal(person.DateOfBirth.ToString("d"),data[3]);
-        Assert.True(System.Convert.ToBoolean(data[4]));
-        Assert.Equal("Single", data[5]);
-    }
-
-    [Fact]
-    public async void CreatePersonWithSpouseTest()
-    {
-        var person = new Person
+        [Fact]
+        public async void CreatePersonTest()
         {
-            FirstName = "Test1",
-            Surname = "Test2",
-            DateOfBirth = new DateTime(1992, 11, 25),
-            RegistrationAllowed = true
-        };
-        var spouse = new Spouse
+            var person = new Person
+            {
+                FirstName = "Test1",
+                Surname = "Test2",
+                DateOfBirth = new DateTime(1992, 11, 25),
+                RegistrationAllowed = true
+            };
+
+            await _dataProvider.CreatePerson(person);
+
+            var personLastLine = File.ReadLines(@"C:\people\mainFile.txt").Last();
+
+            var data = personLastLine.Split("|");
+
+            Assert.False(string.IsNullOrEmpty(data[0]));
+            Assert.Equal(person.FirstName, data[1]);
+            Assert.Equal(person.Surname, data[2]);
+            Assert.Equal(person.DateOfBirth.ToString("d"), data[3]);
+            Assert.True(System.Convert.ToBoolean(data[4]));
+            Assert.Equal("Single", data[5]);
+        }
+
+        [Fact]
+        public async void CreatePersonWithSpouseTest()
         {
-            FirstName = "TestSpouse1",
-            Surname = "TestSpouse2",
-            DateOfBirth = new DateTime(1994, 5, 16)
-        };
+            var person = new Person
+            {
+                FirstName = "Test1",
+                Surname = "Test2",
+                DateOfBirth = new DateTime(1992, 11, 25),
+                RegistrationAllowed = true
+            };
+            var spouse = new Spouse
+            {
+                FirstName = "TestSpouse1",
+                Surname = "TestSpouse2",
+                DateOfBirth = new DateTime(1994, 5, 16)
+            };
 
-        await _dataProvider.CreatePerson(person, spouse);
+            await _dataProvider.CreatePerson(person, spouse);
 
-        var personLastLine = File.ReadLines(@"C:\people\mainFile.txt").Last();
-        var spouseLastLine = File.ReadLines(@"C:\people\spouses\spouses.txt").Last();
+            var personLastLine = File.ReadLines(@"C:\people\mainFile.txt").Last();
+            var spouseLastLine = File.ReadLines(@"C:\people\spouses\spouses.txt").Last();
 
-        var data = personLastLine.Split("|");
+            var data = personLastLine.Split("|");
 
-        Assert.False(string.IsNullOrEmpty(data[0]));
-        Assert.Equal(person.FirstName, data[1]);
-        Assert.Equal(person.Surname, data[2]);
-        Assert.Equal(person.DateOfBirth.ToString("d"), data[3]);
-        Assert.True(System.Convert.ToBoolean(data[4]));
-        Assert.Equal("Married", data[5]);
+            Assert.False(string.IsNullOrEmpty(data[0]));
+            Assert.Equal(person.FirstName, data[1]);
+            Assert.Equal(person.Surname, data[2]);
+            Assert.Equal(person.DateOfBirth.ToString("d"), data[3]);
+            Assert.True(System.Convert.ToBoolean(data[4]));
+            Assert.Equal("Married", data[5]);
 
-        data = spouseLastLine.Split("|");
+            data = spouseLastLine.Split("|");
 
-        Assert.False(string.IsNullOrEmpty(data[0]));
-        Assert.Equal(spouse.FirstName, data[1]);
-        Assert.Equal(spouse.Surname, data[2]);
-        Assert.Equal(spouse.DateOfBirth.ToString("d"), data[3]);
+            Assert.False(string.IsNullOrEmpty(data[0]));
+            Assert.Equal(spouse.FirstName, data[1]);
+            Assert.Equal(spouse.Surname, data[2]);
+            Assert.Equal(spouse.DateOfBirth.ToString("d"), data[3]);
+        }
     }
 }
